0589e69c36120030cfd255052e9d410e95e2163a
anh.tran <anh.tran@tekcent.com>
2022-06-01 13:48:33 +0700

    Add new files

---
+++ GitTraining/GitTraining/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GitTraining
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var greeting = new Greeting();
+            greeting.SayHello();            
+            Console.ReadLine();
+            Console.WriteLine("Press any key to exit.");            
+        }
+    }
+
+    public class Greeting
+    {
+        public void SayHello() 
+        {
+            Console.WriteLine("I am Anh");
+            //Write console your name here.
+            Console.WriteLine("Hi, everyone");
+        }
+    }
+}
